b586993fa8e13bc8adeeaab4e9597ef342ec7ba2
hanna <gasimovahanna@gmail.com>
2021-09-25 08:57:22 +0300

    итог

--- дз1/задача 2.py
+++ дз1/задача 2.py
@@ -3,9 +3,9 @@ b = float(input())
 c = float(input())
 d = float(input())
 f = float(input())
-x = (a*b-c)
-y = (f-d)
+x = a * b - c
+y = f - d
 if y == 0:
     print ('деление на 0 нельзя!')
 else:
-    print(x/y)
+    print(x / y)
