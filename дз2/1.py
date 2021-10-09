4cba22b73b63314c122890a2e316e06615f7e424
hanna <gasimovahanna@gmail.com>
2021-10-09 08:29:25 +0300

    изменение

--- дз2/1.py
+++ дз2/1.py
@@ -1,16 +1,7 @@
-a2 = str(input('введите что-то'))
-<<<<<<< HEAD
+a2 = input('введите что-то')
 b2 = list()
 while a2 != '':
-    a2 = str(input())
+    a2 = input()
     b2.append(a2)
 else:
     print(b2)
-=======
-d2 = list()
-while a2 != '':
-    a2 = str(input())
-    d2.append(a2)
-else:
-    print(d2)
->>>>>>> 919bb33df1d0ea95cbc671e237196cb8d1736216
