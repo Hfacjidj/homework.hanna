8cd96d4aaa6a00ef2b732b65a69172501d5341cb
hanna <gasimovahanna@gmail.com>
2021-11-23 00:28:52 +0300

    bb

--- дз4/дз2.py
+++ дз4/дз2.py
@@ -1,10 +1,9 @@
-def function():
-    n = int(input("Введите n :\n"))
-    if n == 0:
-        return 1
-        factorial = 1
-        while n > 1:
-            factorial *= n
-            n -= 1
-            return factorial
-print(function())
+def factorial(n):
+    x = 1
+    for i in range(2, n + 1):
+        x *= i
+    return x
+n = int(input("Введите число "))
+print(factorial(n))
+assert factorial(0) == 1
+assert factorial(5) == 120
