6e3390d50fabbb125948efa1171886f4b7b9b582
hanna <gasimovahanna@gmail.com>
2021-11-23 09:31:50 +0300

    3.5

--- дз3/задача5.py
+++ дз3/задача5.py
@@ -1,9 +1,9 @@
 def ted(b):
     sr = sum(b) / len(b)
     print('Среднее арифметическое =', sr)
+n = input('ввод ')
 b = list()
-n = (input('Введи  '))
 while n != '':
     b.append(float(n))
-    n = (input('введи '))
+    n = input('введи ')
 ted(b) 
