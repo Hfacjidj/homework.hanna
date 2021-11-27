5242e211163f0cae2897029838be4b55d9dae3b5
hanna <gasimovahanna@gmail.com>
2021-11-27 10:17:35 +0300

    5 hw

---
+++ дз5/бин/poisk.py
@@ -0,0 +1,20 @@
+d = [1, 9, 2, 8, 3, 7, 4, 6, 5]
+d = sorted(d)
+def poisk(d, n):
+    lower = 0
+    upper = len(d) - 1
+    while lower <= upper:
+        center = (lower + upper) // 2
+        if d[center] == n:
+            return center
+        elif d[center] > n:
+            upper = center - 1
+        elif d[center] < n:
+            lower = center + 1
+    return None
+
+
+n = int(input("Элемент, который необходимо обнаружить: "))
+print(poisk(d, n))
+assert (poisk[1, 9, 2, 8, 3, 7, 4, 6, 5], 1) == 0
+assert (poisk[1, 9, 2, 8, 3, 7, 4, 6, 5], 0) == None
