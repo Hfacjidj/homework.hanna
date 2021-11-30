580403c09f15074a4bb20ed5ef1538655171be28
hanna <gasimovahanna@gmail.com>
2021-11-30 09:48:03 +0300

    5

--- дз5/бин/poisk.py
+++ дз5/бин/poisk.py
@@ -1,4 +1,6 @@
 d = [1, 2, 3, 4, 5, 6, 7]
+
+
 def poisk(d, n):
     d = sorted(d)
     lower = 0
@@ -6,6 +8,9 @@ def poisk(d, n):
     while lower <= upper:
         center = (lower + upper) // 2
         if d[center] == n:
+            for m in range(center+1):
+                if d[m] == d[center] and m < center:
+                    center = m
             return center
         elif d[center] > n:
             upper = center - 1
@@ -18,6 +23,6 @@ n = int(input("Элемент, который необходимо обнару�
 print(poisk(d, n))
 assert poisk([1, 2, 3, 4, 5, 6, 7], 5) == 4
 assert poisk([7, 22, 34, 41, 69, 75, 77], 7) == 0
-assert poisk([5, 42, 55, 77, 634, 2345, 33245], 1) == None
-assert poisk([], 1) == None
-assert poisk([7, 7, 7], 7) == 1
+assert poisk([5, 42, 55, 77, 634, 2345, 33245], 1) is None
+assert poisk([], 1) is None
+assert poisk([7, 7, 7], 7) == 0
