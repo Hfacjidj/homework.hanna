31f211ddc04eae94dced1fd84cde3c88de6eae2a
hanna <gasimovahanna@gmail.com>
2022-02-19 12:06:37 +0300

    nk

--- дз1.2/hw1.py
+++ дз1.2/hw1.py
@@ -1,8 +1,16 @@
-from sys import stdin
-class matrix():
-    def __init__(self, spisok): 
-        self.mat = spisok
-    def str(self):
+from msilib import type_localizable
+
+
+class Matrix():
+    def __init__(self, strok = 0, stolb = 0, elements = 0): 
+        self.strok = strok
+        self.stolb = stolb
+        self.elements = elements
+        self.spisok = []
+    def input(self):
+        strok = int(input())
+        stolb = int(input())
+    def __str__(self):
         string = ''
         for i in self.mat:
             for j in i:
@@ -13,7 +21,5 @@ class matrix():
         return string
  
 a = [[1,2,3],[4,5,6],[7,8,9]]
-m = matrix(a)
-print(m.str())
- 
-exec(stdin.read())
+m = Matrix(a)
+print(m)
