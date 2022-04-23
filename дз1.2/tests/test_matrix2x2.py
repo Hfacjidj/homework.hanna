6a2d0c06b8a757ef291ead92bc2a3e28602ab242
hanna <gasimovahanna@gmail.com>
2022-04-23 08:57:22 +0300

    test

--- дз1.2/tests/test_matrix2x2.py
+++ дз1.2/tests/test_matrix2x2.py
@@ -2,6 +2,6 @@ from matrix_app.matrix2x2 import Matrix2x2
 
 
 def test_determinat():
-    a = Matrix2x2([[2, 3], [1,4]])
+    a = Matrix2x2([[2, 3], [1, 4]])
     k = a.determinant()
     assert k == 5
